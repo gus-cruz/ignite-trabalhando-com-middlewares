f0ee2f6aff124c86b6d4466fc47f27d88c57402c
Gus Cruz <gus.fr.cruz@gmail.com>
2022-01-28 22:51:26 -0300

    ✨ finished

--- src/index.js
+++ src/index.js
@@ -10,19 +10,64 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(user => user.username === username)
+
+  if(!user){
+    return response.status(404).json({error: "Username not found!"})
+  }
+
+  request.user = user
+  next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if(!user.pro && user.todos.length === 10){
+    return response.status(403).json({error: "Todo limit exceeded"})
+  }
+
+  next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  const user = users.find(user => user.username === username)
+
+  if(!user){
+    return response.status(404).json({error: "Username not found!"})
+  }
+
+  const checkIsAnUUID = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi
+  if(!checkIsAnUUID.test(id)){
+    return response.status(400).json({error: "Id isn't an uuid!"})
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+  if(!todo){
+    return response.status(404).json({error: "Todo not found!"})
+  }
+
+  request.user = user
+  request.todo = todo
+  next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find(user => user.id === id)
+
+  if(!user){
+    return response.status(404).json({error: "Username not found!"})
+  }
+
+  request.user = user
+  next()
 }
 
 app.post('/users', (request, response) => {
